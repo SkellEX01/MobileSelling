5c9efef65980fd5d3eba759d2dad84d632db1f3d
SkellEX01 <98630434+SkellEX01@users.noreply.github.com>
2022-02-20 16:39:03 +0700

    Update ProductRepository.js

--- product/ProductRepository.js
+++ product/ProductRepository.js
@@ -1,16 +1,20 @@
 const Product = require('./Product');
 
-class ProductRepository {
-    constructor(knex) {
+class ProductRepository 
+{
+    constructor(knex) 
+    {
         this.knex = knex;
     }
 
-    async getAll() {
+    async getAll() 
+    {
         let productList = await this.knex('Type_product').join('Product', 'Type_product.id', '=', 'Product.id_type');
         return productList.map(allProduct => this.factoryProduct(allProduct));
     }
 
-    async findProductById(id) {
+    async findProductById(id) 
+    {
         let rawProduct = await this.knex.select('*').from('Product').where('id', id);
         
         if(rawProduct.length) {
@@ -21,44 +25,52 @@ class ProductRepository {
 
     }
 
-    async findProductGroupBy(groupById){
+    async findProductGroupBy(groupById)
+    {
         let groupProduct = await this.knex.select('*').from('Product').whereIn('id', groupById);
         return groupProduct.map(allProduct => this.factoryProduct(allProduct));
     }
 
-    async findProductByName(name) {
+    async findProductByName(name) 
+    {
         let rawProduct = await this.knex.select('*').from('Product').where('name', 'like', '%'+name+'%');
         return rawProduct.map(allProduct => this.factoryProduct(allProduct));
 
     }
 
-    async findPriceProductGroupBy(groupById) {
+    async findPriceProductGroupBy(groupById) 
+    {
         let groupPrice = await this.knex.from('Product').whereIn('id', groupById);
         return groupPrice.map(allProduct => this.factoryProduct(allProduct));
     }
 
-    async findProductByIdType(id) {
+    async findProductByIdType(id) 
+    {
         let rawProduct = await this.knex.select('*').from('Product').where('id_type', id);
         return rawProduct.map(allProduct => this.factoryProduct(allProduct));
 
     }
 
-    async add(data) {
+    async add(data) 
+    {
         let addProduct = await this.knex('Product').insert(data);
         return addProduct;
     }
 
-    async update(data, id) {
+    async update(data, id) 
+    {
         let updateProduct = await this.knex('Product').where('id', id).update(data);
         return updateProduct;
     }
 
-    async delete(id) {
+    async delete(id) 
+    {
         let deleteProduct = await this.knex('Product').where('id', id).del();
         return deleteProduct;
     }
     
-    factoryProduct(allProductData) {
+    factoryProduct(allProductData) 
+    {
         return new Product(allProductData.id, allProductData.id_type, allProductData.id_promotion, allProductData.name, allProductData.price, allProductData.warranty, allProductData.image, allProductData.description);
     }
 }
