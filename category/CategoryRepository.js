637b9859aca2f90cbd85bcabb324533a910b5dac
SkellEX01 <98630434+SkellEX01@users.noreply.github.com>
2022-02-20 19:33:08 +0700

    Update CategoryRepository.js

--- category/CategoryRepository.js
+++ category/CategoryRepository.js
@@ -1,42 +1,50 @@
 const Category = require('./Category');
 
-class CategoryRepository {
-    constructor(knex) {
+class CategoryRepository 
+{
+    constructor(knex) 
+    {
         this.knex = knex;
     }
     
-    async getAll() {
-        let categoryList = await this.knex.select('*').from('Type_product');
-        return categoryList.map(allCategory => this.factoryCategory(allCategory)); 
+    async getAll() 
+    {
+        let categories = await this.knex.select('*').from('Type_product');
+        return categories.map(category => this.factory(category)); 
     }
 
-    async findCategoryById(id) {
+    async findCategoryById(id) 
+    {
         let rawCategory = await this.knex.select('*').from('Type_product').where('id', id);
 
         if(rawCategory.length) {
-            return this.factoryCategory(rawCategory[0]);
+            return this.factory(rawCategory[0]);
         } 
 
         return null;
     }
 
-    async add(data) {
+    async add(data) 
+    {
         let addCategory = await this.knex('Type_product').insert(data);
         return addCategory;
     }
 
-    async update(data, id) {
+    async update(data, id) 
+    {
         let updateCategory = await this.knex('Type_product').where('id', id).update(data);
         return updateCategory;
     }
 
-    async delete(id) {
+    async delete(id) 
+    {
         let deleteCategory = await this.knex('Type_product').where('id', id).del();
         return deleteCategory;
     }
 
-    factoryCategory(allCategoryData) {
-        return new Category(allCategoryData.id, allCategoryData.name, allCategoryData.description, allCategoryData.status);
+    factory(category) 
+    {
+        return new Category(category);
     }
 }
 
