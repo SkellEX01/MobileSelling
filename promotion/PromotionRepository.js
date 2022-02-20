c2fcf6a5347a70acda11694791e189cced5e55fb
SkellEX01 <98630434+SkellEX01@users.noreply.github.com>
2022-02-20 21:03:35 +0700

    Update PromotionRepository.js

--- promotion/PromotionRepository.js
+++ promotion/PromotionRepository.js
@@ -1,42 +1,50 @@
 const Promotion = require('./Promotion');
 
-class PromotionRepository {
-    constructor(knex) {
+class PromotionRepository 
+{
+    constructor(knex) 
+    {
         this.knex = knex;
     }
 
-    async getAll() {
-        let promotionList = await this.knex.select('*').from('Promotion');
-        return promotionList.map(allPromotion => this.factoryPromotion(allPromotion));
+    async getAll() 
+    {
+        let promotions = await this.knex.select('*').from('Promotion');
+        return promotions.map(promotion => this.factory(promotion));
     }
 
-        async findPromotionById(id) {
-            let rawPromotion = await this.knex.select('*').from('Promotion').where('id', id);
+    async findPromotionById(id) 
+    {
+        let rawPromotion = await this.knex.select('*').from('Promotion').where('id', id);
 
-            if(rawPromotion.length) {
-                return this.factoryPromotion(rawPromotion[0]);
-            }
-
-            return null;
+        if(rawPromotion.length) {
+            return this.factory(rawPromotion[0]);
         }
 
-    async add(data) {
+        return null;
+    }
+
+    async add(data) 
+    {
         let addPromotion = await this.knex('Promotion').insert(data);
         return addPromotion;
     }
 
-    async update(data, id) {
+    async update(data, id) 
+    {
         let updatePromotion = await this.knex('Promotion').where('id', id).update(data);
         return updatePromotion;
     }
 
-    async delete(id) {
+    async delete(id) 
+    {
         let deletePromotion = await this.knex('Promotion').where('id', id).del();
         return deletePromotion;
     }
 
-    factoryPromotion(allPromotion) {
-        return new Promotion(allPromotion.id, allPromotion.name, allPromotion.content);
+    factory(promotion) 
+    {
+        return new Promotion(promotion);
     }
 }
 
