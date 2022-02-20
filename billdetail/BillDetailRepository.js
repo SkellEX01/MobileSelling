aa09162fc6835da94f731a3166fdc07efc4963f6
SkellEX01 <98630434+SkellEX01@users.noreply.github.com>
2022-02-20 19:21:11 +0700

    Update BillDetailRepository.js

--- billdetail/BillDetailRepository.js
+++ billdetail/BillDetailRepository.js
@@ -1,17 +1,21 @@
 const BillDetail = require('./BillDetail');
 
-class BillDetailRepository {
-    constructor(knex) {
+class BillDetailRepository 
+{
+    constructor(knex) 
+    {
         this.knex = knex;
     }
 
-    async getAll() {
-        let billDetailList = await this.knex.select('*').from('Bills_detail');
-        return billDetailList.map(allBillDetail => this.factoryBillDetail(allBillDetail));
+    async getAll() 
+    {
+        let billDetails = await this.knex.select('*').from('Bills_detail');
+        return billDetails.map(billDetail => this.factory(billDetail));
     }
 
-    factoryBillDetail(allBillDetailData) {
-        return new BillDetail(allBillDetailData.id, allBillDetailData.id_bill, allBillDetailData.id_product, allBillDetailData.quantity, allBillDetailData.unit);
+    factory(billDetail) 
+    {
+        return new BillDetail(billDetail);
     }
 }
 
