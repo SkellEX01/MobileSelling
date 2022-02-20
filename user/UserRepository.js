0e263515a9d7aefda7bbe7bf3baf1558fd13c106
SkellEX01 <98630434+SkellEX01@users.noreply.github.com>
2022-02-20 21:10:41 +0700

    Update UserRepository.js

--- user/UserRepository.js
+++ user/UserRepository.js
@@ -1,32 +1,38 @@
 const User = require('./User');
 
-class UserRepository {
-    constructor(knex) {
+class UserRepository 
+{
+    constructor(knex) 
+    {
         this.knex = knex;
     }
 
-    async getAll() {
-        let rawUsers = await this.knex.select('*').from('Admin');
-        return rawUsers.map(rawUser => this.factoryUser(rawUser));
+    async getAll() 
+    {
+        let users = await this.knex.select('*').from('Admin');
+        return users.map(user => this.factory(user));
     }
 
-    async findByUsername(username) {
+    async findByUsername(username) 
+    {
         let [rawUser] = await this.knex.select('*').from('Admin').where('username', username);
-        return !!rawUser ? this.factoryUser(rawUser) : null;
+        return !!rawUser ? this.factory(rawUser) : null;
     }
 
-    async getById(id) {
+    async getById(id) 
+    {
         let rawUser = await this.knex.select('*').from('Admin').where('id', id);
 
         if(rawUser.length) {
-            return this.factoryUser(rawUser[0]);
+            return this.factory(rawUser[0]);
         } 
 
         return null;
     }
 
-    factoryUser(rawUserData) {
-        return new User(rawUserData.id, rawUserData.username, rawUserData.password);
+    factory(user) 
+    {
+        return new User(user);
     }
 }
 
