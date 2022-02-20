8cfca91516793c7c1779dc881368c12ca946f299
SkellEX01 <98630434+SkellEX01@users.noreply.github.com>
2022-02-20 19:22:43 +0700

    Update BillDetail.js

--- billdetail/BillDetail.js
+++ billdetail/BillDetail.js
@@ -1,30 +1,12 @@
-class BillDetail {
-    constructor(id, id_bill, id_product, quantity, unit) {
-        this.id         = id;
-        this.id_bill    = id_bill;
-        this.id_product = id_product;
-        this.quantity   = quantity;
-        this.unit       = unit;
-    }
-
-    getId() {
-        return this.id;
-    }
-
-    getIdBill() {
-        return this.id_bill;
-    }
-
-    getIdProduct() {
-        return this.id_product;
-    }
-
-    getQuantity() {
-        return this.quantity;
-    }
-
-    getUnit() {
-        return this.unit;
+class BillDetail 
+{
+    constructor(billDetail) 
+    {
+        this.id         = billDetail.id;
+        this.id_bill    = billDetail.id_bill;
+        this.id_product = billDetail.id_product;
+        this.quantity   = billDetail.quantity;
+        this.unit       = billDetail.unit;
     }
 }
 
