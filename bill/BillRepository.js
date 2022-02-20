ddc16118fe9684646cb3369ff5f1915dcbcf245e
SkellEX01 <98630434+SkellEX01@users.noreply.github.com>
2022-02-20 19:17:29 +0700

    Update BillRepository.js

--- bill/BillRepository.js
+++ bill/BillRepository.js
@@ -1,30 +1,37 @@
 const Bill = require('./Bill');
 
-class BillRepository {
-    constructor(knex) {
+class BillRepository 
+{
+    constructor(knex) 
+    {
         this.knex = knex;
     }
 
-    async getAll() {
-        let billList = await this.knex.select('*').from('Bills');
-        return billList.map(allBill => this.factoryBill(allBill));
+    async getAll() 
+    {
+        let bills = await this.knex.select('*').from('Bills');
+        return bills.map(bill => this.factory(bill));
     }
 
-    async getIdBill() {
+    async getIdBill() 
+    {
         let id = await this.knex.select('id').from('Bills').orderBy('id', 'desc');
         if(id.length) {
             return this.factoryCustomer(id[0]);
         }
     }
 
-    async add(bill) {
+    async add(bill) 
+    {
         let addBill = await this.knex('Bills').insert(bill);
         return addBill;
     }
 
-    factoryBill(allBillData) {
-        return new Bill(allBillData.id, allBillData.id_customer, allBillData.date_order, allBillData.total, allBillData.payment_status, allBillData.note);
+    factory(bill) 
+    {
+        return new Bill(bill);
     }
+    
 }
 
 module.exports = BillRepository;
